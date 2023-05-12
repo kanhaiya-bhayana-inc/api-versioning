004e83c9e732b9bf024cca7e10d7a22425f9ff33
kanhaiya-bhayana-inc <kanhaiya.bhayana@incedoinc.com>
2023-05-12 19:57:12 +0530

    Init: api-versioning initials :tada:

---
+++ api-versioning-demo/Controllers/UserV2Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_versioning_demo.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserV2Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("User from V2 controller.");
+        }
+    }
+}
---
+++ api-versioning-demo/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// Adding the configuration of API versioning
+builder.Services.AddApiVersioning(x =>
+{
+    x.DefaultApiVersion = new ApiVersion(1, 0);
+    x.AssumeDefaultVersionWhenUnspecified = true;
+    x.ReportApiVersions = true;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
